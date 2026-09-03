0899602ac116ad16baf4e4a938e48e6b1e29e93b
Derin DeNio <derindenio@gmail.com>
2026-09-03 17:30:33 -0600

    Initial project set up before any additions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -21,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} antialiased`}>{children}</body>
+      <body className={cn("antialiased", roboto.variable)}>{children}</body>
     </html>
   );
 }
