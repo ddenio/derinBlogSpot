1a18cd98319342689a04001f19d18b9df26daec6
Derin DeNio <derindenio@gmail.com>
2026-09-05 18:00:01 -0600

    Nav bar layout completed

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -2,12 +2,12 @@ const NavBar = () => {
   return (
     <nav className="sticky top-0 border-b z-50 bg-white">
       <div className="flex justify-between items-center gap-8">
-        <div>
+        <div className="flex items-center gap-1 cursor-pointer">
           <div>Icon</div>
-          <div>Derin's Blog Spot</div>
+          <div>Derins Blog Spot</div>
         </div>
         <div>Search</div>
-        <div>
+        <div className="flex gap-5 sm:gap-8 items-center">
           <div>Theme</div>
           <div>Notifications</div>
           <div>UserMenu</div>
