31322691d081f445cc9b6fd9ae3b632e5f391659
Derin DeNio <derindenio@gmail.com>
2026-09-07 18:07:11 -0600

    Added a Container Component, wrapped navbar with this container component, creates ideal spacing for our navbar items and screensize spacing

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,18 +1,22 @@
+import Container from "./Container";
+
 const NavBar = () => {
   return (
     <nav className="sticky top-0 border-b z-50 bg-white">
-      <div className="flex justify-between items-center gap-8">
-        <div className="flex items-center gap-1 cursor-pointer">
-          <div>Icon</div>
-          <div>Derins Blog Spot</div>
+      <Container>
+        <div className="flex justify-between items-center gap-8">
+          <div className="flex items-center gap-1 cursor-pointer">
+            <div>Icon</div>
+            <div>Derins Blog Spot</div>
+          </div>
+          <div>Search</div>
+          <div className="flex gap-5 sm:gap-8 items-center">
+            <div>Theme</div>
+            <div>Notifications</div>
+            <div>UserMenu</div>
+          </div>
         </div>
-        <div>Search</div>
-        <div className="flex gap-5 sm:gap-8 items-center">
-          <div>Theme</div>
-          <div>Notifications</div>
-          <div>UserMenu</div>
-        </div>
-      </div>
+      </Container>
     </nav>
   );
 };
