270d67add0129753aa732c873da703f636c921ff
Derin DeNio <derindenio@gmail.com>
2026-09-12 19:36:41 -0600

    Added in Shadcn notification components, added in notificaiton bell icon and drop down menu for this

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -2,10 +2,11 @@ import { FaBlogger } from "react-icons/fa";
 import Container from "./Container";
 import ThemeToggle from "./ThemeToggle";
 import SearchInput from "./SearchInput";
+import Notifications from "./Notifications";
 
 const NavBar = () => {
   return (
-    <nav className="sticky top-0 border-b z-50 bg-white dark:bg-slate-950">
+    <nav className="sticky top-0 border-b z-50 bg-background">
       <Container>
         <div className="flex justify-between items-center gap-8">
           <div className="flex items-center gap-1 cursor-pointer">
@@ -15,7 +16,7 @@ const NavBar = () => {
           <SearchInput />
           <div className="flex gap-5 sm:gap-8 items-center">
             <ThemeToggle />
-            <div>Notifications</div>
+            <Notifications />
             <div>UserMenu</div>
           </div>
         </div>
