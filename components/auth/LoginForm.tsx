fa9c37c57c3e28992db750875e2ba8d0cb1c2324
Derin DeNio <derindenio@gmail.com>
2026-09-21 19:13:49 -0600

    Created a Heading reusable component

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -4,7 +4,9 @@ import { LoginSchema, LoginSchemaType } from "@/schemas/LoginSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
 import FormField from "../common/FormField";
-import Button from "../common/button";
+
+import Heading from "../common/Heading";
+import Button from "../common/Button";
 
 const LoginForm = () => {
   const {
@@ -22,6 +24,7 @@ const LoginForm = () => {
       onSubmit={handleSubmit(onSubmit)}
       className="flex flex-col max-w-125 m-auto mt-8 gap-2"
     >
+      <Heading title="Login to DerinSpot.blog" lg center />
       <FormField
         id="email"
         register={register}
